6ba6905990de6fee00edb0ceb5bfa98b2f0928f1
tien199013 <zhao.tian@spreadex.com>
2022-04-12 19:22:20 +0100

    finish chapter 5

---
+++ Chapter5/WorldCitiesAPI/Controllers/SeedController.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OfficeOpenXml;
+using System.Security;
+using WorldCitiesAPI.Data;
+using WorldCitiesAPI.Data.Models;
+
+namespace WorldCitiesAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class SeedController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment _env;
+
+        public SeedController(
+            ApplicationDbContext context,
+            IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Import()
+        {
+            if (!_env.IsDevelopment())
+            {
+                throw new SecurityException("Not allowed");
+            }
+
+            var path = Path.Combine(_env.ContentRootPath, "Data/Source/worldcities.xlsx");
+
+            using var stream = System.IO.File.OpenRead(path);
+            using var excelPackage = new ExcelPackage(stream);
+
+            var worksheet = excelPackage.Workbook.Worksheets[0];
+
+            var nEndRow = worksheet.Dimension.End.Row;
+
+            var numberOfCountriesAdded = 0;
+            var numberOfCitiesAdded = 0;
+
+            var countriesByName = _context.Countries.AsNoTracking()
+                .ToDictionary(x => x.Name, StringComparer.OrdinalIgnoreCase);
+
+            var cities = _context.Cities.AsNoTracking()
+                .ToDictionary(x => (
+                Name: x.Name,
+                Lat: x.Lat,
+                Lon: x.Lon,
+                CountryId: x.CountryId));
+
+            for (var nRow = 2; nRow < nEndRow; nRow++)
+            {
+                var row = worksheet.Cells[nRow, 1, nRow, worksheet.Dimension.End.Column];
+                var countryName = row[nRow, 5].GetValue<string>();
+                if (countriesByName.ContainsKey(countryName))
+                    continue;
+                var iso2 = row[nRow, 6].GetValue<string>();
+                var iso3 = row[nRow, 7].GetValue<string>();
+
+                var country = new Country()
+                {
+                    Name = countryName,
+                    ISO2 = iso2,
+                    ISO3 = iso3,
+                };
+
+                await _context.Countries.AddAsync(country);
+
+                countriesByName.Add(countryName, country);
+
+                numberOfCountriesAdded++;
+            }
+
+            if (numberOfCountriesAdded > 0)
+                await _context.SaveChangesAsync();
+
+            for (var nRow = 2; nRow < nEndRow; nRow++)
+            {
+                var row = worksheet.Cells[nRow, 1, nRow, worksheet.Dimension.End.Column];
+
+                var cityName = row[nRow, 1].GetValue<string>();
+                var nameAscii = row[nRow, 2].GetValue<string>();
+                var lat = row[nRow, 3].GetValue<decimal>();
+                var lon = row[nRow, 4].GetValue<decimal>();
+                var countryName = row[nRow, 5].GetValue<string>();
+
+                var countryId = countriesByName[countryName].Id;
+
+                if (cities.ContainsKey((Name: cityName,
+                Lat: lat,
+                Lon: lon,
+                CountryId: countryId)))
+                    continue;
+
+                var city = new City()
+                {
+                    CountryId = countryId,
+                    Name = cityName,
+                    Lat = lat,
+                    Lon = lon
+                };
+
+                _context.Cities.Add(city);
+
+                numberOfCitiesAdded++;
+            }
+
+
+            if(numberOfCitiesAdded > 0)
+            {
+                await _context.SaveChangesAsync();
+            }
+
+            return new JsonResult(
+                new
+                {
+                    Cities = numberOfCitiesAdded,
+                    Coutries = numberOfCountriesAdded
+                });
+        }
+    }
+}
---
+++ Chapter5/WorldCitiesAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorldCitiesAPI.Data.Models;
+
+namespace WorldCitiesAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<City> Cities => Set<City>();
+        public DbSet<Country> Countries => Set<Country>();
+
+        public ApplicationDbContext() : base()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<City>().ToTable("Cities");
+            modelBuilder.Entity<City>().HasKey(x => x.Id);
+            modelBuilder.Entity<City>().Property(x => x.Id).IsRequired();
+            modelBuilder.Entity<City>().Property(x => x.Lat).HasColumnType("decimal(7,4)");
+            modelBuilder.Entity<City>().Property(x => x.Lon).HasColumnType("decimal(7,4)");
+
+            modelBuilder.Entity<Country>().ToTable("Countries");
+            modelBuilder.Entity<Country>().HasKey(x => x.Id);
+            modelBuilder.Entity<Country>().Property(x => x.Id).IsRequired();
+            modelBuilder.Entity<City>()
+                .HasOne(x => x.Country)
+                .WithMany(y => y.Cities)
+                .HasForeignKey(x => x.CountryId);
+        }
+    }
+}
